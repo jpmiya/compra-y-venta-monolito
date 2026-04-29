825f55204c544ada1b900747f903f920eff5486f
Juampi <jpmiya82@gmail.com>
2026-04-29 16:49:31 -0300

    borro basura de java que quedo

--- app/src/main/java/org/example/app/audit/repository/AuditLogRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.app.audit.repository;
-
-import java.util.UUID;
-
-import org.example.app.audit.domain.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, UUID> {
-}
